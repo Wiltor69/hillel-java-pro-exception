314581f5e6e34fe71a11aacd12cdc9f15a055939
Viacheslav <19wiltor69@gmail.com>
2022-11-25 14:39:36 +0200

    Fixed mistake

--- src/hillel/ArrayValueCalculator.java
+++ src/hillel/ArrayValueCalculator.java
@@ -6,31 +6,30 @@ import hillel.exeption.ArraySizeException;
 
 public class ArrayValueCalculator {
 
-    public  int doCalc (String [][]strings) throws ArraySizeException, NumberFormatException, ArrayDataException,NullPointerException {
+    public  int doCalc (String [][]strings) throws ArraySizeException, NumberFormatException,
+            ArrayDataException {
 
-         int sum = 0;
+        int sum = 0;
 
-        if (strings.length > 4 || strings[0].length > 4){
+        if (strings.length > 4 || strings[0].length > 4) {
             throw new ArraySizeException("The number of elements exceeds the size.");
         } else
-        if (strings == null){
-            throw new ArrayDataException("Problem");
-        }
-        else
+        try {
+            for (int i = 0; i < strings.length; i++) {
+                for (int j = 0; j < strings[0].length; j++) {
 
-        for (int i = 0; i < strings.length; i++) {
-            for (int j = 0; j < strings[0].length; j++) {
+                    sum += Integer.parseInt(strings[i][j]);
 
-                sum += Integer.parseInt(strings[i][j]);
-
-
+                }
             }
+            return sum;
+        }catch (NumberFormatException nf){
+            throw new ArrayDataException("This element is not number");
         }
-                return sum;
+        }
 
     }
 
-}
 
 
 
--- src/hillel/Main.java
+++ src/hillel/Main.java
@@ -5,20 +5,18 @@ import hillel.exeption.ArraySizeException;
 
 public class Main {
     public static void main(String[] args) {
-       try {
+     try {
            ArrayValueCalculator ar = new ArrayValueCalculator();
            String[][] strings = {{"1", "9", "3", "4"}, {"5", "6", "7", "8"}};
            System.out.println("All sum is: " + ar.doCalc(strings));
-       }
-       catch (ArraySizeException ar){
-           System.out.println(ar);
+      }
+      catch (ArraySizeException ar){
+         System.out.println(ar);
        }
        catch (ArrayDataException ad){
-           System.out.println(ad);
+        System.out.println(ad);
        }
-       catch (NumberFormatException nf){
-           System.out.println(nf);
-       }
+
 
 
     }
